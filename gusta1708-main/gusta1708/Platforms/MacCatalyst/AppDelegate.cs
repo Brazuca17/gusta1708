e1077ad9085b7da4f980500d609b839ccd0214a9
Brazuca17 <127317817+Brazuca17@users.noreply.github.com>
2024-03-27 10:27:46 -0300

    Delete gusta1708-main directory

--- gusta1708-main/gusta1708/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace gusta1708;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
